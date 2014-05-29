03313f0ef6b4237a6afd29f4cf8c29b9b9ae2c1f
Michael T. Minogue <michael.minogue@gmail.com>
2014-05-29 14:16:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Ecig::Application.config.secret_key_base = '50bd5922b650c9e20e8a81d7742ec0da1b7cb2829c10771f2c71a5eccc86afbbed0969e15d3544f502e2231e500d4a94d6d9b58f8dcb130a39191cadf36c387c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ecig::Application.config.session_store :cookie_store, key: '_ecig_session'
